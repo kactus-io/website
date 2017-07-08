562b355c690ff004081674c6c8a3dcee29c975e3
Mathieu Dutour <mat.dutour@gmail.com>
2017-07-08 20:57:40 +0200

    add ugly-build

--- getLatestDownloadURL.js
+++ getLatestDownloadURL.js
@@ -1,7 +1,7 @@
 window.fetch('https://api.github.com/repos/kactus-io/kactus/releases/latest')
   .then(function (res) { return res.json() })
   .then(function (res) {
-    const url = res.assets[0].browser_download_url
+    var url = res.assets[0].browser_download_url
 
     document.getElementById('download-url').href = url
   })
